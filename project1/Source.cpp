16601cc7b08d97d54a1d264d98b15004c2927cd5
glof2 <32849073+glof2@users.noreply.github.com>
2020-03-01 01:06:21 +0100

    I removed all the unnecessary IDE files in this one

---
+++ project1/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	bool running = true;
+	int number{ 0 };
+
+	while (running)
+	{
+		std::cout << "Enter a number: ";
+		std::cin >> number;
+		
+		// !number because 0 == false, and it looks better than number == 0
+		if (!number)
+		{
+			running = false;
+		}
+		else
+		{
+			std::cout << number << '\n';
+		}
+	}
+}

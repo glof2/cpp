5dde0a3f05e438b4799f765aa5afb9728b3923ea
glof2 <32849073+glof2@users.noreply.github.com>
2020-03-01 01:13:59 +0100

    Fixed spaces?

--- project1/Source.cpp
+++ project1/Source.cpp
@@ -2,22 +2,22 @@
 
 int main()
 {
-	bool running = true;
-	int number{ 0 };
+  bool running = true;
+  int number{ 0 };
 
-	while (running)
-	{
-		std::cout << "Enter a number: ";
-		std::cin >> number;
-		
-		// !number because 0 == false, and it looks better than number == 0
-		if (!number)
-		{
-			running = false;
-		}
-		else
-		{
-			std::cout << number << '\n';
-		}
-	}
+  while (running)
+  {
+  std::cout << "Enter a number: ";
+  std::cin >> number;
+  
+  // !number because 0 == false, and it looks better than number == 0
+  if (!number)
+  {
+    running = false;
+  }
+  else
+  {
+    std::cout << number << '\n';
+  }
+  }
 }
